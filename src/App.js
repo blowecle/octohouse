a6a6edc3d09d645d9e3e8f658a250ddf84a0cfdb
Brandon Lowe <BLoweCLE@gmail.com>
2023-01-17 11:39:09 -0600

    config for heroku deployment

--- src/App.js
+++ src/App.js
@@ -1,72 +1,84 @@
 import './css/App.css';
-import { BrowserRouter } from 'react-router-dom';
-import Router from './components/Routes';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import Home from './components/Home';
+import React, { useState, useEffect } from 'react';
 
+function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [scrollPosition, setScrollPosition] = useState(0);
 
-const App = () => {
+  useEffect(() => {
+    function handleScroll() {
+      setScrollPosition(window.pageYOffset);
+      console.log(scrollPosition)
+      if (scrollPosition >= 100) {
+        setCurrentIndex(1);
+      }
+
+      if (scrollPosition >= 500) {
+        setCurrentIndex(2);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrollPosition]);
+
   return (
-    <BrowserRouter>
-      {/* <Router /> */}
-      <div className='wrapper'>
-        {/* <Parallax pages={7}
-        style={{maxHeight: '100vh'}}>
-          <ParallaxLayer
-          speed={.4}
-          style={{backgroundImage: `url('https://res.cloudinary.com/ddqp7dojc/image/upload/v1671217017/octo-house/Layer_7_ro2xih.png')`, backgroundSize: 'cover'}}>
-          </ParallaxLayer>
-          <ParallaxLayer
-          speed={.5}
-          style={{backgroundImage: `url('https://res.cloudinary.com/ddqp7dojc/image/upload/v1671217017/octo-house/Layer_6_stvbn0.png')`, backgroundSize: 'cover'}}>
-          </ParallaxLayer>
-          <ParallaxLayer
-          speed={.7}
-          style={{backgroundImage: `url('https://res.cloudinary.com/ddqp7dojc/image/upload/v1671217017/octo-house/Layer_5_yyyrmp.png')`, backgroundSize: 'cover'}}>
-          </ParallaxLayer>
-          <ParallaxLayer
-          speed={.7}
-          style={{backgroundImage: `url('https://res.cloudinary.com/ddqp7dojc/image/upload/v1671217017/octo-house/Layer_4_ckfbaz.png')`, backgroundSize: 'cover'}}>
-          </ParallaxLayer>
-          <ParallaxLayer
-          speed={.8}
-          style={{backgroundImage: `url('https://res.cloudinary.com/ddqp7dojc/image/upload/v1671217017/octo-house/Layer_3_gnidiu.png')`, backgroundSize: 'cover'}}>
-          </ParallaxLayer>
-          <ParallaxLayer
-          speed={.9}
-          style={{backgroundImage: `url('https://res.cloudinary.com/ddqp7dojc/image/upload/v1671217017/octo-house/Layer_2_rqykmh.png')`, backgroundSize: 'cover'}}>
-          </ParallaxLayer>
-          <ParallaxLayer
-          speed={1}
-          style={{backgroundImage: `url('https://res.cloudinary.com/ddqp7dojc/image/upload/v1671217017/octo-house/Layer_1_wz0wl7.png')`, backgroundSize: 'cover'}}>
-          </ParallaxLayer>
-          <ParallaxLayer
-          offset={.9}
-          speed={1}
-          style={{backgroundColor: '#8BD0DB'}}>
-            <div>
-              <h2>offset</h2>
-            </div>
-          </ParallaxLayer>
-        </Parallax> */}
-        <div className='parallax-wrapper'>
-          <img className='parallax-one' src='https://res.cloudinary.com/ddqp7dojc/image/upload/v1671217017/octo-house/Layer_7_ro2xih.png'/>
-          <img className='parallax-two' src='https://res.cloudinary.com/ddqp7dojc/image/upload/v1671217017/octo-house/Layer_6_stvbn0.png'/>
-          <img className='parallax-three' src='https://res.cloudinary.com/ddqp7dojc/image/upload/v1671217017/octo-house/Layer_5_yyyrmp.png'/>
-          <img className='parallax-four' src='https://res.cloudinary.com/ddqp7dojc/image/upload/v1671217017/octo-house/Layer_4_ckfbaz.png'/>
-          <img className='parallax-five' src='https://res.cloudinary.com/ddqp7dojc/image/upload/v1671217017/octo-house/Layer_3_gnidiu.png'/>
-          <img className='parallax-six' src='https://res.cloudinary.com/ddqp7dojc/image/upload/v1671217017/octo-house/Layer_2_rqykmh.png'/>
-          <img className='parallax-seven' src='https://res.cloudinary.com/ddqp7dojc/image/upload/v1671217017/octo-house/Layer_1_wz0wl7.png'/>
-        </div>
-        <div className='content'>
-          <div className='image-wrapper'>
-            <img className='tester-image' src='https://res.cloudinary.com/ddqp7dojc/image/upload/v1668440619/octo-house/-2303878664998712562_ghe5fy.jpg'/>
-          </div>
-          <div className='filler'></div>
-          
-        </div>
-      </div>
-    </BrowserRouter>
+    <Home/>
+    // <div className='content'>
+    //   <div
+    //     className={`sliding-div ${currentIndex >= 0 ? 'visible' : 'hidden'}`}
+    //     style={{ backgroundColor: 'red' }}
+    //   >
+    //     Div 1
+    //   </div>
+    //   <div
+    //     className={`sliding-div ${currentIndex >= 1 ? 'visible' : 'hidden'}`}
+    //     style={{ backgroundColor: 'green' }}
+    //   >
+    //     Div 2
+    //   </div>
+    //   <div
+    //     className={`sliding-div ${currentIndex >= 2 ? 'visible' : 'hidden'}`}
+    //     style={{ backgroundColor: 'blue' }}
+    //   >
+    //     Div 3
+    //   </div>
+    // </div>
   );
 }
+// import { BrowserRouter } from 'react-router-dom';
+// import Router from './components/Routes';
+// import {Parallax} from 'react-parallax'
+// import first from './first.jpeg'
+// import second from './second.jpeg'
+// import third from './third.jpeg'
+
+// const App = () => {
+  // return (
+  //   <BrowserRouter>
+      {/* <Router /> */}
+      {/* <Parallax strength={200} bgImage={first}>
+        <div className="content">
+          <div className="text-content">Normal Parallax</div>
+        </div>
+      </Parallax>
+
+      <Parallax strength={300} blur={{ min: -5, max: 15 }} bgImage={second}>
+        <div className="content">
+          <div className="text-content">Blur</div>
+        </div>
+      </Parallax>
+
+      <Parallax strength={-600} bgImage={third}>
+        <div className="content">
+          <div className="text-content">Reverse Parallax</div>
+        </div>
+      </Parallax>
+    </BrowserRouter>
+  ); */}
+// }
 
 export default App;
