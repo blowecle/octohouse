5d67d753587ed9bdce45b2a8b6e61856ce0313ee
Brandon Lowe <BLoweCLE@gmail.com>
2023-02-14 16:41:23 -0600

    setting up heroku postgresql database

--- server/db/connection.js
+++ server/db/connection.js
@@ -8,8 +8,9 @@ if (process.env.QUIET) {
 
 //you name this whatever your project is
 const connection = new Sequelize(
-	process.env.DATABASE_URL || 'postgres://localhost/octohouse',
-	config
+	process.env.DATABASE_URL || 'postgres://mfbvinptdryhxd:86c4d257e150a21691fdf8a645025fe3f8faabb07a9a56eb4c37d394291d3ffa@ec2-3-229-252-6.compute-1.amazonaws.com:5432/dakbufup5id0be',
+	// process.env.DATABASE_URL || 'postgres://localhost/octohouse',
+    config
 );
 
 module.exports = connection;
