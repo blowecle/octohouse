1deebcaf3c8f217a20e60253cd9b2fa049d2d601
Brandon Lowe <BLoweCLE@gmail.com>
2023-02-24 15:35:55 -0600

    single artist view and api updates

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -4,17 +4,19 @@ import Home from "./artifact/Home";
 import Blog from "./blog/Blog";
 import Story from "./story/Story";
 import ArtifactView from "./artifact/ArtifactView";
+import ArtistView from "./artists/ArtistView";
 
 const Router = () => {
 
     return (
 
         <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/artifacts" element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/artifacts" element={<Home />} />
             <Route path="/artifacts/:id" element={<ArtifactView />} />
+            <Route path="/artists/:id" element={<ArtistView/>} />
             <Route path="/story" element={<Story />} />
-            <Route path="/blog" element={<Blog />}/>
+            <Route path="/blog" element={<Blog />} />
         </Routes>
     )
 }
