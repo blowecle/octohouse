0658db54cae3a94622a2bc114c0c28bf92d3cb9c
Brandon Lowe <BLoweCLE@gmail.com>
2023-03-01 13:08:41 -0600

    added routes for /nelson and /heidi

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -5,6 +5,8 @@ import Blog from "./blog/Blog";
 import Story from "./story/Story";
 import ArtifactView from "./artifact/ArtifactView";
 import ArtistView from "./artists/ArtistView";
+import Nelson from "./nelson/Nelson";
+import Heidi from "./heidi/Heidi";
 
 const Router = () => {
 
@@ -16,6 +18,8 @@ const Router = () => {
             <Route path="/artists/:id" element={<ArtistView/>} />
             <Route path="/story" element={<Story />} />
             <Route path="/blog" element={<Blog />} />
+            <Route path="/nelson" element={<Nelson />} />
+            <Route path="/heidi" element={<Heidi />} />
         </Routes>
     )
 }
