2c761b6901695ba08c90cb7aa6925f181afbb1a3
Brandon Lowe <BLoweCLE@gmail.com>
2023-03-01 13:19:51 -0600

    added storySliceReducer to configureStore within store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,14 @@ import loggerMiddleware from "redux-logger";
 import { artifactSliceReducer } from './reducers/artifactSlice';
 import { artistSliceReducer } from './reducers/artistSlice';
 import { postSliceReducer } from './reducers/postSlice';
+import { storySliceReducer } from './reducers/storySlice';
 
 export default configureStore({
     reducer: {
         artifact: artifactSliceReducer,
         artist: artistSliceReducer,
-        post: postSliceReducer
+        post: postSliceReducer,
+        story: storySliceReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(loggerMiddleware),
