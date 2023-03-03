18e2417c893f7026edf8843c29e1320a1e0d08f5
Brandon Lowe <BLoweCLE@gmail.com>
2023-03-03 10:31:41 -0500

    fetching all artifacts and all artists on home page.  Filtering artists before passing them to Artifact.js component as props

--- src/components/artifact/Artifact.js
+++ src/components/artifact/Artifact.js
@@ -5,40 +5,20 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchArtifactData } from '../../store/reducers/artifactSlice';
 
-const Artifact = ({artifact, index}) => {
+const Artifact = ({artifact, artists}) => {
 
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        const asyncFetchArtifactData = async () => {
-            await dispatch(fetchArtifactData(index+1));
-        }
-        asyncFetchArtifactData();
-    }, [index])
-
-    
-    
-    const artifactData = useSelector((state) => state.artifact.artifactData);
-
-    if(artifactData.artists) { 
-    console.log("ARTIFACT DATA: ", artifactData.artifact)
-    console.log("ARTIFACT ARTIST DATA: ", artifactData.artists)
-    }
+    console.log("INSIDE ARTIFACT.JS, ARTIFACT: ", artifact, " ARTIST: ", artists)
 
     return (
-        <div className='artifact-container'>
-            {console.log("INSIDE RETURN ARTIFACT: ", artifactData.artifact, " ARTIST: ", artifactData.artists)}
-            {artifactData.artifact ? (<div>{`${artifactData.artifact[0].name}`}</div>) : (null)}
-            {artifactData.artists ? (artifactData.artifact[0].artistDescription.map((description, index) => (<>
-                    <div>{`${artifactData.artists[index].name}`}</div>
-                    <div>{`${description}`}</div>
+        <div className="artifact-container">
+            <div>{`${artifact.name}`}</div>
+            {artists.map((artist, index) => (<>
+                <div>{`${artist.name}`}</div>
+                <div>{`${artifact.artistDescription[index]}`}</div>
                 </>
-            ))    ) : (null)
-            
-            }
+            ))}
         </div>
     )
-
     // return (<div className='artifact-container'>
     //             <div className='post-image-wrapper'>
     //                 {artifact.images.map((image) => (
