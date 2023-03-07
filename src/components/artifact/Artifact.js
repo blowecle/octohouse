fb3b46f3d8ad2fe729e88e06594d3000f8e2cee1
Brandon Lowe <BLoweCLE@gmail.com>
2023-03-07 17:46:20 -0600

    automated image carousels for artifact gallery

--- src/components/artifact/Artifact.js
+++ src/components/artifact/Artifact.js
@@ -1,24 +1,23 @@
+import React from 'react';
 import '../../css/artifact.css';
 import { Link } from 'react-router-dom';
 
 const Artifact = ({artifact, artists}) => {
 
-    console.log("INSIDE ARTIFACT.JS, ARTIFACT: ", artifact, " ARTIST: ", artists)
+
 
     return (
         <div className="artifact-container">
-            <div className='post-image-wrapper'>
-                {artifact.images.map((image) => (
-                    <div className='inner-wrapper'>
-                        <img src={image} alt='blogImage' className='artifact-image'/>
+                    <div className='slides'>
+                        {artifact.images.map((image) => (
+                            <img src={image} alt='artifactImage' className='slide'/>
+                        ))}
                     </div>
-                ))}
-                <div className="icon" aria-hidden="true"></div>
-            </div>
             <div className="info-wrapper">
                 <Link to={`/artifacts/${artifact.artifactID}`}>
                     <div className="artifact-name">{`${artifact.name}`}</div>
                 </Link>
+                <div className="line-div"/>
                 {artists.map((artist, index) => (<>
                     <Link to={`/artists/${artist.artistID}`}>
                         {artist.name ? (
