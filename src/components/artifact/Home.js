4e1fa5f03bb8677ba351795a701c26b64bdcdd6d
Brandon Lowe <BLoweCLE@gmail.com>
2023-03-07 14:24:10 -0600

    modified home page, removed extra links

--- src/components/artifact/Home.js
+++ src/components/artifact/Home.js
@@ -25,14 +25,6 @@ const Home = () => {
     
     return (<>
             <div className='home-container'>
-                <div className='temp-wrapper'>
-                    <Link to='/blog'>
-                        <div className='temp-link'>Blog</div>
-                    </Link>
-                    <Link to='/story'>
-                        <div className='temp-link'>Story</div>
-                    </Link>
-                </div>
                 <Slider/>
                 <div className="welcome">
                     Welcome to the Octopus House gallery, feel free to scroll through each gallery, and click the artists' name for more info!
@@ -41,21 +33,13 @@ const Home = () => {
                     const filteredArtists = artists.filter((artist) => artist.artifactID.includes(artifact.artifactID))
                     return <Artifact key={artifact.id} artifact={artifact} artists={filteredArtists}/>
                 })) : null}
-                <div className='temp-wrapper'>
-                    <Link to='/blog'>
-                        <div className='temp-link'>Blog</div>
-                    </Link>
-                    <Link to='/story'>
-                        <div className='temp-link'>Story</div>
-                    </Link>
-                </div>
-            <div className='before-after-slider'>
+            {/* <div className='before-after-slider'>
                 <ReactCompareSlider
                 itemOne={<ReactCompareSliderImage src='https://res.cloudinary.com/ddqp7dojc/image/upload/v1673983927/octo-house/PXL_20230117_193004917_fzax1v.jpg' />}
                 itemTwo={<ReactCompareSliderImage src='https://res.cloudinary.com/ddqp7dojc/image/upload/v1668440619/octo-house/-8773391783772082014_cbis9x.jpg' />}
                 position={1}
                 />
-            </div>
+            </div> */}
         </div>
     </>
   )
