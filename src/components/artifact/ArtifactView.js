55d7fc9626706f7a4e7f36dd169c703b43b11669
Brandon Lowe <BLoweCLE@gmail.com>
2023-03-13 17:41:34 -0500

    added custom background

--- src/components/artifact/ArtifactView.js
+++ src/components/artifact/ArtifactView.js
@@ -21,11 +21,6 @@ const ArtifactView = () => {
     if(artifactData.artists) { artifactData.artists.map((artist) => {console.log("params id: ", params.id, " and artist id: ", artist.id)})}
     console.log("ARTIFACT DATA: ", artifactData.artifact)
     console.log("ARTIFACT ARTIST DATA: ", artifactData.artists)
-    let artists = []
-
-    //due to eager loading I probably no longer need the 2 lines below
-    // if(artifactData.artists) {artists = artifactData.artists.filter((artist) => artist.id === parseInt(params.id))}
-    // console.log("FILTERED ARTIFACT ARTIST DATA: ", artists)
 
     return (
         <div className="single-artifact-container">
