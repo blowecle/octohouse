b8d834a93cadacfcdd3821c0755f507bfff5d843
Brandon Lowe <BLoweCLE@gmail.com>
2023-03-14 11:07:56 -0500

    changes to image carousel

--- src/components/artifact/Artifact.js
+++ src/components/artifact/Artifact.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import '../../css/artifact.css';
 import { Link } from 'react-router-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const Artifact = ({artifact, artists}) => {
+    
 
     return (
         <div className="artifact-container">
-                    <div className='slides'>
+                    {/* <div className='slides'>
                         {artifact.images.map((image) => (
                             <img src={image} alt='artifactImage' className='slide'/>
                         ))}
-                    </div>
+                    </div> */}
+                    <Carousel showThumbs={false} showStatus={false} showIndicators={true} infiniteLoop={true} autoPlay={false} swipeable={true} emulateTouch={true} dynamicHeight={true} useKeyboardArrows={true} centerMode={true} centerSlidePercentage={100} className='artifact-carousel'>
+                        {artifact.images.map((image) => (
+                            <div className='inner-wrapper'>
+                                <img src={image} alt='artifactImage' className='artifact-image'/>
+                                </div>
+                        ))}
+                    </Carousel>
             <div className="info-wrapper">
                     <div className="artifact-name">{`${artifact.name}`}</div>
                 <div className="line-div"/>
                 <div className="artifact-artist-wrapper">
                 {artists.map((artist, index) => (<>
                         <div className="artifact-artist-description">{`${artifact.artistDescription[index]}`}</div>
-                    <Link to={`/artists/${artist.artistID}`}>
+                    {/* <Link to={`/artists/${artist.artistID}`}> */}
                         {artist.name ? (
                         <div className="artifact-artist-name">{`${artist.name}`}</div>
                         ) : (
                             <div className="artifact-artist-name">{`${artist.company}`}</div>
                         )}
-                    </Link>
+                    {/* </Link> */}
                     </>
                 ))}
                 </div>
