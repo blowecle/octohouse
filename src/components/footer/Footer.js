556366df243d042dad8453b9a08b482af546b684
Brandon Lowe <BLoweCLE@gmail.com>
2023-03-17 10:29:45 -0500

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,28 +1,30 @@
 import React from "react";
+import '../../css/footer.css';
 
 const Footer = () => {
     return (
-        <footer>
             <div className="footer-container">
-            <div className="footer-artist-container">
-                    <div className="footer-artist-description">ENGINEERED BY</div>
-                    <div className="footer-artist">Brandon Lowe</div>
-                    <i className="fas fas-github"></i>
+                <div className="footer-artists">
+                    <div className="footer-artist-container">
+                        <div className="footer-artist-description">PHOTOS BY</div>
+                        <div className="footer-artist">JAMES RUIZ</div>
+                        <div className="footer-artist">PERRY HALL</div>
+                    </div>
+                    <div className="footer-artist-container">
+                        <div className="footer-artist-description">WEBSITE BY</div>
+                        <div className="footer-artist">BRANDON LOWE</div>
+                    </div>
+                    <div className="footer-artist-container">
+                        <div className="footer-artist-description">DESIGN BY</div>
+                        <div className="footer-artist">EVAN FARIAS</div>
+                    </div>
+                    <div className="footer-artist-container">
+                        <div className="footer-artist-description"></div>
+                        <div className="footer-artist"></div>
+                    </div>
                 </div>
-                <div className="footer-artist-container">
-                    <div className="footer-artist-description"></div>
-                    <div className="footer-artist"></div>
-                </div>
-                <div className="footer-artist-container">
-                    <div className="footer-artist-description"></div>
-                    <div className="footer-artist"></div>
-                </div>
-                <div className="footer-artist-container">
-                    <div className="footer-artist-description"></div>
-                    <div className="footer-artist"></div>
-                </div>
+                <div className="footer-info">1005 S. 3RD ST AUSTIN, TX 78704</div>
             </div>
-        </footer>
     )
 }
 
