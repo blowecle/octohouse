bc9a8fbc5f6a633d2d42b637e54ccb94c815d9ff
Brandon Lowe <BLoweCLE@gmail.com>
2023-05-16 13:00:05 -0500

    initial desktop style

--- src/components/artifact/Home.js
+++ src/components/artifact/Home.js
@@ -39,13 +39,14 @@ const Home = () => {
     
     return (<>
             <div className='home-container'>
-                <img className='home-image' src="https://res.cloudinary.com/dyjzfdguj/image/upload/v1679084549/evan%20web%20photos/Top-100_jfqnl0.jpg"/>
+                <img className='home-image-mobile' src="https://res.cloudinary.com/dyjzfdguj/image/upload/v1679084549/evan%20web%20photos/Top-100_jfqnl0.jpg"/>
+                <img className='home-image-desktop' src="https://res.cloudinary.com/dyjzfdguj/image/upload/v1684249771/Desktop_TOP_v2-100_ykbouo.jpg"/>
                 <div className="gallery-container">
                     {artifacts ? (artifacts.map((artifact, index) => {
                         const filteredArtists = artists.filter((artist) => artist.artifactID.includes(artifact.artifactID))
                         return <Artifact key={index} artifact={artifact} artists={filteredArtists}/>
                     })) : null}
-                <div className="last-artifact-container">
+                <div className="artifact-container">
                 <div className='before-after-slider'>
                 <ReactCompareSlider
                 itemOne={<ReactCompareSliderImage src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1679086343/evan%20web%20photos/PXL_20230317_204739350_vpu26f.jpg' />}
@@ -54,11 +55,13 @@ const Home = () => {
                 />
             </div>
             <div className="info-wrapper">
-                <div className="last-artifact-name">THE TRANSITION</div>
-                <div className="last-line-div"/>
+                <div className="artifact-name">THE TRANSITION</div>
+                <div className="line-div"/>
                 <div className="artifact-blurb">Many thanks to the dozens of artists, craftspeople, and contractors that transformed a boring gray cube into a true work of art!</div>
-                <div className="last-description">CREATED BY</div>
-                <div className="last-name">Everybody</div>
+                <div className="artifact-artist-wrapper">
+                    <div className="artifact-artist-description">CREATED BY</div>
+                    <div className="last-name">Everybody</div>
+                </div>
             </div>
             </div>
         </div>
