9f86d9290914595ce38e9eafa85671d6f628a953
Brandon Lowe <BLoweCLE@gmail.com>
2023-05-28 10:24:57 -0500

    test

--- server/api/artifactartist.js
+++ server/api/artifactartist.js
@@ -1,16 +1,17 @@
-const router = require('express').Router();
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
-import ArtifactArtist from '../db/ArtifactArtist';
+// const router = require('express').Router();
+// const Sequelize = require('sequelize');
+// const Op = Sequelize.Op;
+// const { ArtifactArtist } = require('../db');
 
-//GET /api/artifactartist cross-reference table
-router.get('/', async(req, res, next) => {
-    try {
-        const artifactArtist = await ArtifactArtist.findAll();
-        res.send(artifactArtist)
-    } catch (e) {
-        next(e);
-    }
-});
 
-module.exports = router;
+// //GET /api/artifactartist cross-reference table
+// router.get('/artifactartist', async(req, res, next) => {
+//     try {
+//         const artifactArtist = await ArtifactArtist.findAll();
+//         res.send(artifactArtist)
+//     } catch (e) {
+//         next(e);
+//     }
+// });
+
+// module.exports = router;
--- src/App.js
+++ src/App.js
@@ -7,36 +7,5 @@ function App() {
 
   return <Router/>;
 }
-// import { BrowserRouter } from 'react-router-dom';
-// import Router from './components/Routes';
-// import {Parallax} from 'react-parallax'
-// import first from './first.jpeg'
-// import second from './second.jpeg'
-// import third from './third.jpeg'
-
-// const App = () => {
-  // return (
-  //   <BrowserRouter>
-      {/* <Router /> */}
-      {/* <Parallax strength={200} bgImage={first}>
-        <div className="content">
-          <div className="text-content">Normal Parallax</div>
-        </div>
-      </Parallax>
-
-      <Parallax strength={300} blur={{ min: -5, max: 15 }} bgImage={second}>
-        <div className="content">
-          <div className="text-content">Blur</div>
-        </div>
-      </Parallax>
-
-      <Parallax strength={-600} bgImage={third}>
-        <div className="content">
-          <div className="text-content">Reverse Parallax</div>
-        </div>
-      </Parallax>
-    </BrowserRouter>
-  ); */}
-// }
 
 export default App;
