5cc85d233b9892620002711f6d6aa7d7004b6871
Brandon Lowe <BLoweCLE@gmail.com>
2023-05-28 10:36:32 -0500

    adding artifactArtist api route

--- server/api/artifactartist.js
+++ server/api/artifactartist.js
@@ -1,17 +1,17 @@
-// const router = require('express').Router();
-// const Sequelize = require('sequelize');
-// const Op = Sequelize.Op;
-// const { ArtifactArtist } = require('../db');
+const router = require('express').Router();
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const { ArtifactArtist } = require('../db');
 
 
-// //GET /api/artifactartist cross-reference table
-// router.get('/artifactartist', async(req, res, next) => {
-//     try {
-//         const artifactArtist = await ArtifactArtist.findAll();
-//         res.send(artifactArtist)
-//     } catch (e) {
-//         next(e);
-//     }
-// });
+//GET /api/artifactartist cross-reference table
+router.get('/artifactartist', async(req, res, next) => {
+    try {
+        const artifactArtist = await ArtifactArtist.findAll();
+        res.send(artifactArtist)
+    } catch (e) {
+        next(e);
+    }
+});
 
-// module.exports = router;
+module.exports = router;
