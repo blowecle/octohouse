bd254ff59cb04afaf7cd8853cfcf58eb43b66101
Brandon Lowe <BLoweCLE@gmail.com>
2023-07-24 17:49:35 -0500

    conditional rendering solution for single artist

--- src/components/artists/ArtistView.js
+++ src/components/artists/ArtistView.js
@@ -3,25 +3,48 @@ import { fetchArtistData } from "../../store/reducers/artistSlice";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import '../../css/artist.css'
+import { fetchArtifactArtist } from "../../store/reducers/artifactArtistSlice";
+import { fetchArtifacts } from "../../store/reducers/artifactSlice";
 
 const ArtistView = () => {
 
     const params = useParams();
     const dispatch = useDispatch();
 
-    useEffect(() => {
-        const asyncFetchArtistData = async () => {
-            await dispatch(fetchArtistData(params.id));
-        }
-        asyncFetchArtistData();
-    }, [dispatch])
-
-    const artist = useSelector((state) => state.artist.artistData.artistData);
-    const artifacts = useSelector((state) => state.artist.artistData.artifacts)
     
+    
+    
+    const artist = useSelector((state) => state.artist.artistData.artistData);
+    const artifactArtist = useSelector((state) => state.artifactArtist.artifactArtist)
+    const artifacts = useSelector((state) => state.artifact.artifacts);
+
+    let artistsArtifacts;
+    let singleArtifact;
+    let imagePath;
+    if(artist){
+        console.log(artist)
+        if(artifactArtist.length > 0 && artist.artistID){
+            artistsArtifacts = artifactArtist.filter((artifact) => artifact.artistID.includes(parseInt(artist.artistID)))
+            singleArtifact = artifacts.filter((artifact) => artifact.artifactID === artistsArtifacts[0].artifactID)
+            if(singleArtifact[0]){
+                imagePath = require(`../../images/${singleArtifact[0].images[0]}`)
+            }
+        }
+    }
+
+
+        useEffect(() => {
+            const asyncFetchData = async () => {
+                await dispatch(fetchArtistData(params.id));
+                await dispatch(fetchArtifactArtist());
+                await dispatch(fetchArtifacts())
+            }
+            asyncFetchData();
+        }, [])
+
     return (<div className="artist-page">
-    {artist ? (<div className="artist-wrapper">
-        <img src={`../../images/${artifacts[0].images[0]}`} alt="this is where the main image goes" className="artist-main-image"/>
+    {artist && imagePath ? (<div className="artist-wrapper">
+        <img src={imagePath} alt="this is where the main image goes" className="artist-main-image"/>
         {artist.company && artist.name ? (<>
                 <div className="single-artist-name">{`${artist.name}`}</div>
                 <div className="single-artist-title">{`${artist.title}`}</div>
