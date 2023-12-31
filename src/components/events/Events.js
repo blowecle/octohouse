b953bf570fbdc126799fffee15dba772b81ee775
Brandon Lowe <BLoweCLE@gmail.com>
2023-12-31 12:19:57 -0600

    removed three.js

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -1,19 +1,11 @@
 import React, { useState, useRef } from 'react';
-import ThreeScene from './ThreeScene';
 import '../../css/three.css';
 
 const Events = () => {
-    const container = useRef();
-    const [cameraIndex, setCameraIndex] = useState(0);
-
-    const handleButtonClick = () => {
-        setCameraIndex(prevIndex => prevIndex + 1);
-    };
 
     return (
-        <div className="three_bg" ref={container}>
-            <button className="test_button" onClick={handleButtonClick}>Switch Camera</button>
-            <ThreeScene cameraIndex={cameraIndex} container={container}/>
+        <div>
+            This is where the events will go
         </div>
     );
 };
