f4414784e51bd5dfd748d77ece65bf7b8c164232
Brandon Lowe <BLoweCLE@gmail.com>
2024-02-18 12:11:46 -0600

    route and code clean up

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./artifact/Home";
-import Blog from "./blog/Blog";
-import Story from "./story/Story";
 import ArtistView from "./artists/ArtistView";
 import Events from "./events/Events";
 
@@ -13,8 +11,6 @@ const Router = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/artists/:id" element={<ArtistView/>} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/story" element={<Story />} />
             <Route path="/events" element={<Events />} />
         </Routes>
     )
