b60f00486dd82c3588a6a2faee7f549e17800529
Brandon Lowe <BLoweCLE@gmail.com>
2024-03-07 12:44:38 -0600

    events page work: mobile header complete

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -6,18 +6,14 @@ const Events = () => {
 
     return (
         <section className="events-container">
-            <div className="header-container">
-                <div className="header-inner-container">
-                    <div className="header-title">
-                        RENT THE OCTOPUS HOUSE!
-                    </div>
-                    <div className="header-content">
-                    If you have made it this far then you know you can host your own event at the Octopus House!  Take time exploring the space to see if this house is the right fit for you and your team.
-                    </div>
-                </div>
-            </div>
-            <div className="event-spaces">
-
+            <img className="events-image-mobile" src="https://res.cloudinary.com/dyjzfdguj/image/upload/v1709836127/octopus-house/eventsHeaderMobile_vpgznw.jpg"/>
+            <img className="events-image-tablet"/>
+            <img className="events-image-desktop"/>
+            <div className="events-blurb-container">
+                <div className="events-header">RENT THE OCTOPUS HOUSE!</div>
+                <div className="events-blurb">{`Impress your friends, family and colleagues by hosting an\n
+                experience they will never forget.  This playful home caters to a wide variety\n
+                of gatherings with 4 uniquge spaces and options to upgrade your stay.`}</div>
             </div>
         </section>
     );
