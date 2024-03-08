cbc751233c028d5ed6ed4dec1e4a5442a7820d66
Brandon Lowe <BLoweCLE@gmail.com>
2024-03-08 12:56:53 -0600

    another form tweak

--- src/components/events/Contact.js
+++ src/components/events/Contact.js
@@ -2,36 +2,36 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <form method="POST" data-netlify="true" name="contact" className='contact-form'>
-        <input
-        type="hidden"
-        name="form-name"
-        value="contact"
-        />
-        <input
-        className='name-input'
-        name="name"
-        type="text"
-        placeholder="Your name"
-        required
-        />
-        <input
-        className='email-input'
-        name="email"
-        type="email"
-        placeholder="Your email"
-        required
-        />
-        <textarea
-        className='message-input'
-        name="message"
-        placeholder="Further information"
-        required
-        ></textarea>
-        <div className='button-wrapper'>
-        <button type="submit">Send</button>
-        </div>
-    </form>
+<form method="POST" data-netlify="true" name="contact" className='contact-form'>
+    <input
+    type="hidden"
+    name="form-name"
+    value="contact"
+    />
+    <input
+    className='name-input'
+    name="name"
+    type="text"
+    placeholder="Your name"
+    required
+    />
+    <input
+    className='email-input'
+    name="email"
+    type="email"
+    placeholder="Your email"
+    required
+    />
+    <textarea
+    className='message-input'
+    name="message"
+    placeholder="Further information"
+    required
+    ></textarea>
+    <div className='button-wrapper'>
+      <button type="submit">Send</button>
+    </div>
+</form>
   )
 }
 
