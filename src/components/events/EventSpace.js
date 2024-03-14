8543fe11e25ca34c75d4180b650092bb253de8e5
Brandon Lowe <BLoweCLE@gmail.com>
2024-03-14 12:06:47 -0500

    pushing events page up to test on mobile

--- src/components/events/EventSpace.js
+++ src/components/events/EventSpace.js
@@ -8,28 +8,29 @@ const EventSpace = () => {
         <div className="event-space-inner-container">
             <div className="event-space-header">
                 <div className="event-space-name">
-                    MAIN ROOM
+                    <strong>MAIN ROOM</strong>
                 </div>
                 <div className="event-space-nickname">
-                    {`(Under The Eyes of Texas)`}
+                    <strong>{`(Under The Eyes of Texas)`}</strong>
                 </div>
             </div>
             <div className="event-space-content-container">
-                <img src={require('../../images/interior1.jpeg')} alt="main-room" className="event-space-image"/>
+                <img src="https://res.cloudinary.com/dyjzfdguj/image/upload/v1710435898/octopus-house/OctopusHouse_PH_368-min_gd4ejp.jpg" alt="main-room" className="event-space-image"/>
                 <div className="event-space-content-container">
                     <div className="event-space-content-title">
-
+                    Great space for conversation, catching a game, or a party!
                     </div>
                     <div className="event-space-content">
-                        <div className="event-space-content-item">-  Restrooms available</div>
-                        <div className="event-space-content-item">-  Bar & Lounge Seating</div>
-                        <div className="event-space-content-item">-  Seats 8-12</div>
-                        <div className="event-space-content-item">-  TV + Music System</div>
-                        <div className="event-space-content-item">-  Party Lights</div>
+                        <div className="event-space-content-item">{`*  Restrooms available`}</div>
+                        <div className="event-space-content-item">{`*  Bar & Lounge Seating`}</div>
+                        <div className="event-space-content-item">{`*  Seats 8-12`}</div>
+                        <div className="event-space-content-item">{`*  TV + Music System`}</div>
+                        <div className="event-space-content-item">{`*  Party Lights`}</div>
                     </div>
                 </div>
             </div>
         </div>
+        <img className="wave-break" src="https://res.cloudinary.com/dyjzfdguj/image/upload/v1709837172/octopus-house/Wave_line_break_ibyp0o.png" alt="wave-break"/>
     </div>
   )
 }
--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -11,22 +11,20 @@ const Events = () => {
     return (
         <section className="events-container">
             <img className="events-image-mobile" src="https://res.cloudinary.com/dyjzfdguj/image/upload/v1709836127/octopus-house/eventsHeaderMobile_vpgznw.jpg"/>
-            <img className="events-image-tablet"/>
-            <img className="events-image-desktop"/>
+            <img className="events-image-tablet" src="https://res.cloudinary.com/dyjzfdguj/image/upload/v1710433847/octopus-house/eventsHeaderTablet_ie6zkw.jpg"/>
+            <img className="events-image-desktop" src="https://res.cloudinary.com/dyjzfdguj/image/upload/v1710433872/octopus-house/eventsHeaderDesktop_pw8qki.jpg"/>
             <div className="events-blurb-container">
                 <div className="events-header">RENT THE OCTOPUS HOUSE!</div>
                 <div className="events-blurb">Impress your friends, family and colleagues by hosting an
                 experience they will never forget.  This playful home caters to a wide variety
-                of gatherings with 4 uniquge spaces and options to upgrade your stay.</div>
+                of gatherings with 4 unique spaces and options to upgrade your stay.</div>
             </div>
             <img className="wave-break" src="https://res.cloudinary.com/dyjzfdguj/image/upload/v1709837172/octopus-house/Wave_line_break_ibyp0o.png" alt="wave-break"/>
             <div className="event-spaces-container">
-
+                <EventSpace />
             </div>
-            <img className="wave-break" src="https://res.cloudinary.com/dyjzfdguj/image/upload/v1709837172/octopus-house/Wave_line_break_ibyp0o.png" alt="wave-break"/>
-            {/* this is a temporary wave-break */}
             <div className="upgrades-container">
-                <div className="upgrades-header">* UPGRADE YOUR STAY *</div>
+                <div className="upgrades-header">{`* UPGRADE YOUR STAY *`}</div>
                 <div className="upgrades-inner-container">
                     <div className="upgrade">
                         <img className="upgrade-icon" src="https://res.cloudinary.com/dyjzfdguj/image/upload/v1709838380/octopus-house/Chef_icon_nus3ig.png" alt="upgrade-icon"/>
@@ -40,17 +38,15 @@ const Events = () => {
                         <img className="upgrade-icon" src="https://res.cloudinary.com/dyjzfdguj/image/upload/v1709838380/octopus-house/Entertainment_icon_nobo6j.png" alt="upgrade-icon"/>
                         <div className="upgrade-info-container">
                             <div className="upgrade-header">ENTERTAINMENT</div>
-                            <div className="upgrade-blurb">Want to add some flare to your event?<br/>
-                            Check out this list of entertainers for\nthat perfect vibe.</div>
+                            <div className="upgrade-blurb">Want to add some flare to your event?
+                            Check out this list of entertainers for that perfect vibe.</div>
                         </div>
                     </div>
                     <div className="upgrade">
                         <img className="upgrade-icon" src="https://res.cloudinary.com/dyjzfdguj/image/upload/v1709838380/octopus-house/Stay_the_Night_icon_h3aruu.png" alt="upgrade-icon"/>
                         <div className="upgrade-info-container">
                             <div className="upgrade-header">STAY THE NIGHT</div>
-                            <div className="upgrade-blurb">Are you from out-of-town or not ready for the<br/>
-                            fun to end after 1 day? 4 bedrooms available<br/>
-                            for your extended-stay needs.</div>
+                            <div className="upgrade-blurb">Are you from out-of-town or not ready for the fun to end after 1 day? 4 bedrooms available for your extended-stay needs.</div>
                         </div>
                     </div>
                 </div>
@@ -62,26 +58,7 @@ const Events = () => {
             <img className="wave-break" src="https://res.cloudinary.com/dyjzfdguj/image/upload/v1709837172/octopus-house/Wave_line_break_ibyp0o.png" alt="wave-break"/>
             <div className="additional-info-container">
                 <div className="additional-inner-container">
-                    <div className="info-div">ADDITIONAL INFORMATION
-                        <ul className="package-list">
-                            <li className="list-item">Wifi</li>
-                            <li className="list-item">Grill</li>
-                            <li className="list-item">Restrooms</li>
-                            <li className="list-item">Hot Tub</li>
-                            <li className="list-item">Sound System</li>
-                            <li className="list-item">Party Light</li>
-                            <li className="list-item">Kitchen</li>
-                        </ul>
-                    </div>
-                    <div className="info-div">Max Capacity: 50 ppl</div>
-                    <div className="info-div">House Rules</div>
-                </div>
-            </div>
-            <div className="additional-info-container">
-                <div className="additional-inner-container">
-                    <div className="additional-info-header">
-                        ADDITIONAL INFORMATION
-                    </div>
+                    <div className="additional-info-header">ADDITIONAL INFO</div>
                     <div className="additional-info-one">
                         <div className="info-header">Included in this package:</div>
                         <ul className="package-list">
@@ -91,7 +68,7 @@ const Events = () => {
                             <li className="list-item">Hot Tub</li>
                             <li className="list-item">Sound System</li>
                             <li className="list-item">Party Light</li>
-                            <li className="list-item">Kitchen</li>
+                            <li className="list-item last-item">Kitchen</li>
                         </ul>
                     </div>
                     <div className="additional-info-two">
