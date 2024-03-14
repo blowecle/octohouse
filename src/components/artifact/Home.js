6b85e0d4c7d7661c790db53872365a128c152352
Brandon Lowe <BLoweCLE@gmail.com>
2024-03-14 15:48:19 -0500

    test

--- src/components/artifact/Home.js
+++ src/components/artifact/Home.js
@@ -14,6 +14,7 @@ import { fetchArtifactArtist } from '../../store/reducers/artifactArtistSlice';
 import { setInitialDataLoaded } from '../../store/reducers/artifactSlice';
 
 const Home = () => {
+  console.log('hello from Home')
     const dispatch = useDispatch();
 
     const [imagesLoaded, setImagesLoaded] = useState(false);
