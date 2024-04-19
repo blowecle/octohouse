067673af4a0c9a1f6ffa871963f1fa7fe205c5e4
Brandon Lowe <BLoweCLE@gmail.com>
2024-04-19 16:29:32 -0500

    test

--- server/api/spaces.js
+++ server/api/spaces.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
-const { Spaces } = require('../db');
+const { Space } = require('../db');
 
 
 //GET all spaces
 router.get('/', async(req, res, next) => {
     try {
         console.log('inside api')
-        const spaceList = await Spaces.findAll();
+        const spaceList = await Space.findAll();
         res.send(spaceList);
     } catch (e) {
         next(e);
